d0a53da91cacd2074b66353dd0c784c91f68ada8
Jan Jongen <jan.jongen@valtech.com>
2024-11-01 18:33:59 +0100

    Sample text working

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,14 @@ use panic_probe as _; // global logger
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use ssd1306::{mode::DisplayConfig, prelude::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306};
-use stm32f4xx_hal::{gpio::GpioExt, i2c::{I2c, Mode}, pac, rcc::RccExt, time::{Hertz, KiloHertz}};
+use stm32f4xx_hal::{gpio::GpioExt, i2c::{I2c, Mode}, pac, rcc::RccExt, time::Hertz};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+
 
 #[entry]
 fn main() -> ! {
@@ -33,6 +40,21 @@ fn main() -> ! {
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
     
     loop {
         // your code goes here
